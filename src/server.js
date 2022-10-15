e2e099f0b7298bcb257f182c5d7e8976b777f363
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-15 07:33:42 -0300

    remove comentarios

--- src/server.js
+++ src/server.js
@@ -1,8 +1,4 @@
 const app = require('./app');
 require('dotenv/config');
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
-
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
-});
+app.listen(process.env.PORT, () => console.log(`Escutando na porta ${process.env.PORT}`));
